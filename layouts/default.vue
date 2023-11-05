4711e7285c384274a7cbf333031cabb71eecdb26
julien huang <julien.huang@outlook.fr>
2023-11-05 16:45:42 +0100

    fix style

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,11 +12,17 @@
 
       <SideBar class="hidden lg:grid" />
     </div>
-    <main class="lg:w-5/6">
-      <button class=" lg:hidden text-4xl ml-4 " @click="isOpen = !isOpen">
-        <Icon name="ci:hamburger" />
-      </button>
-      <slot />
+    <main class="lg:w-5/6 w-full flex flex-col">
+      <div>
+        <button class=" lg:hidden text-4xl ml-4 " @click="isOpen = !isOpen">
+          <Icon name="ci:hamburger" />
+        </button>
+      </div>
+
+      <div class="flex-1 w-full">
+        <slot />
+
+      </div>
     </main>
 
   </div>
