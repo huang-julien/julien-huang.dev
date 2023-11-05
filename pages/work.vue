330e7c5739144b0c075e11332e88a4ab764390c8
julien huang <julien.huang@outlook.fr>
2023-11-05 17:08:01 +0100

    fix: change style

--- pages/work.vue
+++ pages/work.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="lg:px-4 h-full flex w-full">
+  <div class="px-4 h-full flex w-full">
     <div class="mt-5 lg:my-auto w-full">
       <MainTitle class="w-1/2 md:w-1/4">Work</MainTitle>
       <section class="my-5">
         <h2 class="text-2xl text-p-yellow font-bold">Projects</h2>
-        <div class="mx-auto md:px-[4rem]  w-3/4 lg:w-full">
+        <div class="mx-auto md:px-[4rem] w-full ">
           <ProjectsList :projects="myProjects" />
         </div>
 
       </section>
       <section class="my-5">
         <h2 class="text-2xl text-p-yellow font-bold">Actively contributing to</h2>
-        <div class="mx-auto md:px-[4rem]  w-3/4  lg:w-full">
+        <div class="mx-auto md:px-[4rem] w-full">
           <ProjectsList :projects="contributingToProjects" />
         </div>
       </section>
