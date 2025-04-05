b34a5d042436572d41a99de772b2090a1089010f
Julien Huang <julien.h.dev@gmail.com>
2025-04-05 22:59:56 +0200

    feat: add error page

---
+++ app/error.vue
@@ -0,0 +1,22 @@
+<template>
+    <NuxtLayout name="default">
+        <section class="my-auto grid grid-cols-2 gap-4">
+            <div class="font-bold text-5xl text-right">
+                {{ error?.statusCode }}
+            </div>
+            <div class="text-3xl">
+                <h2>
+                    {{ error?.message }}
+                </h2>
+
+                <UButton to="/">
+                    Go home
+                </UButton>
+            </div>
+        </section>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const error = useError()
+</script>
--- app/pages/[...content].vue
+++ app/pages/[...content].vue
@@ -1,16 +1,23 @@
 <template>
     <div class="my-auto h-full">
-        <ContentRenderer v-if="home" :value="home" />
+        <ContentRenderer v-if="content" :value="content" />
     </div>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
-const { data: home } = await useAsyncData(() => queryCollection('content').path(route.path).first())
+const { data: content } = await useAsyncData(() => queryCollection('content').path(route.path).first())
   
 useSeoMeta({
-  title: home.value?.title,
-  description: home.value?.description
+  title: content.value?.title,
+  description: content.value?.description
 })
+
+if(!content.value) {
+    showError({
+        statusCode: 404,
+        message: 'Page not found'
+    })
+}
 </script>
  
