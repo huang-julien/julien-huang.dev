9330dbf3e1097cf38d76c398854e7b23d47ef267
Julien Huang <julien.h.dev@gmail.com>
2025-09-07 17:50:15 +0200

    chore: add cv

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -18,6 +18,9 @@
                 <li class="my-5">
                     <NuxtLink to="/work">Work</NuxtLink>
                 </li>
+                <li class="my-5">
+                    <NuxtLink to="/cv" :prefetch="false">CV</NuxtLink>
+                </li>
             </ul>
         </nav>
 
